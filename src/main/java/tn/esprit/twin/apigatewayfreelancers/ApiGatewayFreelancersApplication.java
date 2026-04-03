fd36405c04dd9c56dd7bf2c9012d786fc4e6b526
nour bouslimi <bouslimi.nourelhouda@esprit.tn>
2026-04-03 11:04:15 +0100

    ajout de token relay et oauth2-client dependency

--- src/main/java/tn/esprit/twin/apigatewayfreelancers/ApiGatewayFreelancersApplication.java
+++ src/main/java/tn/esprit/twin/apigatewayfreelancers/ApiGatewayFreelancersApplication.java
@@ -20,10 +20,14 @@ public class ApiGatewayFreelancersApplication {
     public RouteLocator getRoutes(RouteLocatorBuilder builder){
         return builder.routes().route(
                         //id mission c le nom de la route sur le controller de mission
-                        "mission",r->r.path("/freelancerProject/mission/**").uri("lb://freelancerProject") //lb: load balance //lb://freelancerProject : le nom de l'application mission nour njibha men application.properties
+                        "mission",r->r.path("/freelancerProject/mission/**")
+                                .filters(f -> f.tokenRelay()) // ← AJOUTE CECI
+                                .uri("lb://freelancerProject") //lb: load balance //lb://freelancerProject : le nom de l'application mission nour njibha men application.properties
                 ).route(
 
-                        "reviews",r->r.path("/reviews/**").uri("lb://ReviewRating") //lb: load balance //lb://freelancerProject : le nom de l'application mission nour njibha men application.properties
+                        "reviews",r->r.path("/reviews/**")
+                                .filters(f -> f.tokenRelay()) // ← AJOUTE CECI
+                                .uri("lb://ReviewRating") //lb: load balance //lb://freelancerProject : le nom de l'application mission nour njibha men application.properties
                 ).route(
 
                         "api/contracts",r->r.path("/api/contracts/**").uri("lb://Contract") //lb: load balance //lb://freelancerProject : le nom de l'application mission nour njibha men application.properties
